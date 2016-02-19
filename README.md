6cc5e215d12bac79c76f78ca7548393819cef733
Keith <keithmiller@umass.edu>
2016-02-18 23:04:36 -0500

    Removed pyc, Fixed AnswerChecker, Edited README

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # KeithTeachesPython
 The Repo for a Python workshop taught by Keith Miller for the ACM Cybersecurity Club
+
+## Requirements
+* Python2.7.X
+* IPython
+* Jupyter
+
+## Files
+* Syntax is a lesson on basic Python syntax
+* Lesson 1 takes on some of the easier Project Euler Problems
--- lesson1.py
+++ lesson1.py
@@ -16,7 +16,7 @@ class AnswerChecker(object):
     
     def check(self, question, answer):
         corr_ans = self.answer_key[question]     
-        if answer is corr_ans:
+        if answer == corr_ans:
             print "CORRECT!"
             return True
         elif type(answer) is not type(corr_ans):
@@ -29,3 +29,18 @@ class AnswerChecker(object):
 
 if __name__=='__main__':
     print "This module is meant to be imported in the Lesson1 IPython Notebook"
+    
+    ac = AnswerChecker()
+    
+    # Question 1
+    ac.check(0, 42)
+    ac.check(0, 43)
+
+    
+    # Question 2
+    ac.check(1, 233168)
+    ac.check(1, 233169)
+
+    # Question 3
+    ac.check(2, 4613732)
+    ac.check(2, 4613733)
